a738101521962d7a19d21e5fdc63d29d959e801c
pswbuild <pswbuild@oppo.com>
2025-01-15 11:00:25 +0800

    Synchronize code for OnePlus PKG110_15.0.0.301(CN01) Based on QCOM release TAG:AU_LINUX_KERNEL.PLATFORM.3.0.R1.00.00.00.017.100

--- kernel/fork.c
+++ kernel/fork.c
@@ -23,6 +23,7 @@
 #include <linux/sched/task.h>
 #include <linux/sched/task_stack.h>
 #include <linux/sched/cputime.h>
+#include <linux/sched/ext.h>
 #include <linux/seq_file.h>
 #include <linux/rtmutex.h>
 #include <linux/init.h>
@@ -868,8 +869,35 @@ static void check_mm(struct mm_struct *mm)
 #endif
 }
 
+#ifndef CONFIG_CONT_PTE_HUGEPAGE
 #define allocate_mm()	(kmem_cache_alloc(mm_cachep, GFP_KERNEL))
 #define free_mm(mm)	(kmem_cache_free(mm_cachep, (mm)))
+#else
+#define __allocate_mm()	(kmem_cache_alloc(mm_cachep, GFP_KERNEL))
+#define __free_mm(mm)	(kmem_cache_free(mm_cachep, (mm)))
+
+static inline void *allocate_mm(void)
+{
+	struct mm_struct *mm = __allocate_mm();
+
+	if (unlikely(!mm))
+		return NULL;
+
+	mm->android_kabi_reserved1 = (u64)kzalloc(sizeof(struct chp_vma_name_address),
+						  GFP_KERNEL);
+	if (!mm->android_kabi_reserved1) {
+		__free_mm(mm);
+		return NULL;
+	}
+	return mm;
+}
+
+static inline void free_mm(struct mm_struct *mm)
+{
+	kfree((void *)(mm->android_kabi_reserved1));
+	__free_mm(mm);
+}
+#endif
 
 /*
  * Called when the last reference to the mm
@@ -932,6 +960,7 @@ void __put_task_struct(struct task_struct *tsk)
 	WARN_ON(refcount_read(&tsk->usage));
 	WARN_ON(tsk == current);
 
+	sched_ext_free(tsk);
 	io_uring_free(tsk);
 	cgroup_free(tsk);
 	task_numa_free(tsk, true);
@@ -1278,12 +1307,21 @@ fail_nopgd:
 struct mm_struct *mm_alloc(void)
 {
 	struct mm_struct *mm;
+#ifdef CONFIG_CONT_PTE_HUGEPAGE
+	unsigned long rsv1;
+#endif
 
 	mm = allocate_mm();
 	if (!mm)
 		return NULL;
 
+#ifdef CONFIG_CONT_PTE_HUGEPAGE
+	rsv1 = mm->android_kabi_reserved1;
+#endif
 	memset(mm, 0, sizeof(*mm));
+#ifdef CONFIG_CONT_PTE_HUGEPAGE
+	mm->android_kabi_reserved1 = rsv1;
+#endif
 	return mm_init(mm, current, current_user_ns());
 }
 
@@ -1628,12 +1666,23 @@ static struct mm_struct *dup_mm(struct task_struct *tsk,
 {
 	struct mm_struct *mm;
 	int err;
+#ifdef CONFIG_CONT_PTE_HUGEPAGE
+	unsigned long rsv1;
+#endif
 
 	mm = allocate_mm();
 	if (!mm)
 		goto fail_nomem;
 
+#ifdef CONFIG_CONT_PTE_HUGEPAGE
+	rsv1 = mm->android_kabi_reserved1;
+#endif
 	memcpy(mm, oldmm, sizeof(*mm));
+#ifdef CONFIG_CONT_PTE_HUGEPAGE
+	memcpy((void *)rsv1, (void *)oldmm->android_kabi_reserved1,
+	       sizeof(struct chp_vma_name_address));
+	mm->android_kabi_reserved1 = rsv1;
+#endif
 
 	if (!mm_init(mm, tsk, mm->user_ns))
 		goto fail_nomem;
@@ -2343,7 +2392,7 @@ static __latent_entropy struct task_struct *copy_process(
 
 	retval = perf_event_init_task(p, clone_flags);
 	if (retval)
-		goto bad_fork_cleanup_policy;
+		goto bad_fork_sched_cancel_fork;
 	retval = audit_alloc(p);
 	if (retval)
 		goto bad_fork_cleanup_perf;
@@ -2484,7 +2533,9 @@ static __latent_entropy struct task_struct *copy_process(
 	 * cgroup specific, it unconditionally needs to place the task on a
 	 * runqueue.
 	 */
-	sched_cgroup_fork(p, args);
+	retval = sched_cgroup_fork(p, args);
+	if (retval)
+		goto bad_fork_cancel_cgroup;
 
 	/*
 	 * From this point on we must avoid any synchronous user-space
@@ -2530,13 +2581,13 @@ static __latent_entropy struct task_struct *copy_process(
 	/* Don't start children in a dying pid namespace */
 	if (unlikely(!(ns_of_pid(pid)->pid_allocated & PIDNS_ADDING))) {
 		retval = -ENOMEM;
-		goto bad_fork_cancel_cgroup;
+		goto bad_fork_core_free;
 	}
 
 	/* Let kill terminate clone/fork in the middle */
 	if (fatal_signal_pending(current)) {
 		retval = -EINTR;
-		goto bad_fork_cancel_cgroup;
+		goto bad_fork_core_free;
 	}
 
 	/* No more failure paths after this point. */
@@ -2612,10 +2663,11 @@ static __latent_entropy struct task_struct *copy_process(
 
 	return p;
 
-bad_fork_cancel_cgroup:
+bad_fork_core_free:
 	sched_core_free(p);
 	spin_unlock(&current->sighand->siglock);
 	write_unlock_irq(&tasklist_lock);
+bad_fork_cancel_cgroup:
 	cgroup_cancel_fork(p, args);
 bad_fork_put_pidfd:
 	if (clone_flags & CLONE_PIDFD) {
@@ -2654,6 +2706,8 @@ bad_fork_cleanup_audit:
 	audit_free(p);
 bad_fork_cleanup_perf:
 	perf_event_free_task(p);
+bad_fork_sched_cancel_fork:
+	sched_cancel_fork(p);
 bad_fork_cleanup_policy:
 	lockdep_free_task(p);
 #ifdef CONFIG_NUMA
